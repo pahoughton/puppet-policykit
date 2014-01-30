ca029bab6c4dbb1cdd6e829eb8349fad777cd6b2
Paul Houghton <paul_houghton@cable.comcast.com>
2014-01-30 14:06:52 -0700

    WIP - add fedora support.

--- manifests/params.pp
+++ manifests/params.pp
@@ -15,18 +15,17 @@ class policykit::params {
   $policykit_local_path = '/etc/polkit-1/localauthority/50-local.d'
 
   case $::osfamily {
-    'redhat': {
-      case $::operatingsystemmajrelease {
-        6: {
-          $policykit_package = 'polkit'
-        }
-        default: {
-          fail("Module ${module_name} is not supported on operatingsystemmajrelease ${::operatingsystemmajrelease}")
-        }
-      }
-    }
     'debian': {
       $policykit_package = 'policykit-1'
+    }
+    'redhat': {
+      if $::operatingsystem == 'Fedora' {
+        $policykit_package = 'polkit'
+      } elsif $::operatingsystemmajrelease == 6 {
+        $policykit_package = 'polkit'
+      } else {
+        fail("Module ${module_name} is not supported on operatingsystemmajrelease ${::operatingsystemmajrelease}")
+      }
     }
     default: {
       fail("Module ${module_name} is not supported on ${::operatingsystem}")
